17a60fd1355c59087a41ad448b9aec670bc95217
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2021-06-02 15:04:06 +0530

    portfolio v1.0

---
+++ gatsby-portfolio-dev/src/data/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  defaultTitle: 'Kaustuv Karan',
+  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  author: 'Kaustuv Karan',
+  url: 'https://kaustuvkaran01.netlify.app/',
+  legalName: 'Kaustuv Karan',
+  defaultDescription: 'I’m John and I’m a Backend & Devops engineer!',
+  socialLinks: {
+    twitter: 'http://www.twitter.com/smakosh',
+    github: 'https://github.com/smakosh',
+    linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
+    instagram: 'https://instagram.com/smakosh19',
+    youtube: 'https://www.youtube.com/user/smakoshthegamer',
+    google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+  },
+  googleAnalyticsID: 'UA-88875900-4',
+  themeColor: '#6b63ff',
+  backgroundColor: '#6b63ff',
+  social: {
+    facebook: 'appId',
+    twitter: '@smakosh',
+  },
+  address: {
+    city: 'City',
+    region: 'Region',
+    country: 'Country',
+    zipCode: 'ZipCode',
+  },
+  contact: {
+    email: 'kaustuvkaran@gmail.com',
+    phone: '+918059645585',
+  },
+  foundingDate: '2021',
+};
