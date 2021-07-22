2d90ab9617f9b75cd473b9d5893136ab76c38d69
kaustuvkaran01 <kaustuvkaran01@gmail.com>
2021-07-22 21:03:32 +0530

    chore: notable project section made dynamic

--- portfolio-v1/src/components/SideProjects.js
+++ portfolio-v1/src/components/SideProjects.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React from "react";
+
+import { FaExternalLinkAlt } from "react-icons/fa";
+import { VscGithubAlt } from "react-icons/vsc";
 
 export default function SideProjects() {
+
+  const projects = [
+    {
+      "title":"First project",
+      "desc":"Something random is better than nothing at all but help me god for I am dying",
+      "github":"https://www.kaustuvkaran01.netlify.app",
+      "live":"https://www.kaustuvkaran01.netlify.app"
+    },
+    {
+      "title":"First project",
+      "desc":"Something random is better than nothing at all but help me god for I am dying",
+      "github":"https://www.kaustuvkaran01.netlify.app",
+      "live":"https://www.kaustuvkaran01.netlify.app"
+    },
+    {
+      "title":"First project",
+      "desc":"Something random is better than nothing at all but help me god for I am dying",
+      "github":"https://www.kaustuvkaran01.netlify.app",
+      "live":"https://www.kaustuvkaran01.netlify.app"
+    },
+    {
+      "title":"First project",
+      "desc":"Something random is better than nothing at all but help me god for I am dying",
+      "github":"https://www.kaustuvkaran01.netlify.app",
+      "live":"https://www.kaustuvkaran01.netlify.app"
+    },
+    {
+      "title":"First project",
+      "desc":"Something random is better than nothing at all but help me god for I am dying",
+      "github":"https://www.kaustuvkaran01.netlify.app",
+      "live":"https://www.kaustuvkaran01.netlify.app"
+    }
+  ];
+
   return (
     <>
-    {/* TODO: Add the side projects and the videos for the side projects */}
-    <section class="text-gray-600 body-font lg:mx-28">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col">
-      <div class="h-1 bg-gray-200 rounded overflow-hidden">
-        <div class="w-24 h-full bg-indigo-500"></div>
-      </div>
-      <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
-        <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">Other Notable Projects</h1>
-        <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">Street art subway tile salvia four dollar toast bitters selfies quinoa yuccie synth meditation iPhone intelligentsia prism tofu. Viral gochujang bitters dreamcatcher.</p>
-      </div>
-    </div>
-    <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
-      <div class="p-4 lg:w-1/3">
+      {/* TODO: Add the side projects and the videos for the side projects */}
+      <section class="text-gray-600 body-font lg:mx-28">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-col">
+            <div class="h-1 bg-gray-200 rounded overflow-hidden">
+              <div class="w-24 h-full bg-indigo-500"></div>
+            </div>
+            <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
+              <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">
+                Other Notable Projects
+              </h1>
+              <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">
+                Street art subway tile salvia four dollar toast bitters selfies
+                quinoa yuccie synth meditation iPhone intelligentsia prism tofu.
+                Viral gochujang bitters dreamcatcher.
+              </p>
+            </div>
+          </div>
+          <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+              <div class="flex flex-wrap -m-4 justify-evenly">
+                {/* <div class="p-4 lg:w-1/3">
         <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
           <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
           <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Raclette Blueberry Nextious Level</h1>
@@ -43,8 +86,8 @@ export default function SideProjects() {
             </span>
           </div>
         </div>
-      </div>
-      <div class="p-4 lg:w-1/3">
+      </div> */}
+                {/* <div class="p-4 lg:w-1/3">
         <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
           <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
           <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Ennui Snackwave Thundercats</h1>
@@ -69,38 +112,58 @@ export default function SideProjects() {
             </span>
           </div>
         </div>
-      </div>
-      <div class="p-4 lg:w-1/3">
-        <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-          <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">Selvage Poke Waistcoat Godard</h1>
-          <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
-          <a href="https://www.kaustuvkaran01.netlify.app"   class="text-indigo-500 inline-flex items-center">Learn More
-            <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
-          <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-            <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                <circle cx="12" cy="12" r="3"></circle>
-              </svg>1.2K
-            </span>
-            <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-              <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>6
-            </span>
-          </div>
+      </div> */}
+      {projects.map((project,i) => {
+        return (
+          
+                <div class="p-4 lg:w-1/3">
+                  <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                    {/* <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
+                    <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                      {project.title}
+                    </h1>
+                    <p class="leading-relaxed mb-3 text-left">
+                      {project.desc}
+                    </p>
+                    <a
+                      href={project.github}
+                      class="text-indigo-500 inline-flex items-center"
+                    >
+                      Learn More
+                      <svg
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                    <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                      <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-400">
+                        <a href={project.github}>
+                          <VscGithubAlt size={20} />
+                        </a>
+                      </span>
+                      <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                        <a href={project.live}>
+                          <FaExternalLinkAlt />
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+        )
+      })}
+              </div>
+            </div>
+          </section>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
-  </div>
-</section>
+      </section>
     </>
   );
 }
